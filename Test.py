a20ba7ddfdfa398bdea50d917a3cf2c53734e37e
Rohit-Nakhate <rohit.nakhate@a-devops>
2023-04-23 14:10:42 +0530

    initial commit

---
+++ Test.py
@@ -0,0 +1,18 @@
+
+def add (a,b):
+    add = a/b
+    print(add)
+    return a,b
+
+def sub (c,d):
+    sub = c-d
+    print(sub)
+    return c,d
+
+def main():
+    add(110,20)
+    sub(80,10)
+
+
+if __name__=='__main__':
+    main()
